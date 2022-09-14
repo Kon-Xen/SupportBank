d0f6bbb2e95ba31f91b00cda45457bb8c0d2a64e
Konstantinos <kon.xen@:gmail.com>
2022-09-14 17:08:17 +0100

    addition of Account class and

---
+++ Account.cs
@@ -0,0 +1,13 @@
+namespace Bank
+{
+    class Account
+    {
+        private int id;
+        private string name;
+        private List<object> owesTo;  // list of ids
+        private List<object> owesFrom; // list of ids
+        private DateTime dateOf;
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+/* todo:
+*We open the file
+ * use contents to create accounts ( loop )
+
+ * put the accounts in a list to be ready to query.
+
+ * List All - prints out the names of each person, along with the total amount they owe or are owed, as before
+    Mary owes 1000, is owned 50
+
+ * List [Account] -prints out every transaction(with date, narrative, to and amount) for the specific account the user asks for.
+
+        Mary | 12 / 09 / 2014 | pokemon training | people list of what is owed to | people list of what is owed from
+        calculate diff as Disha pointed out!
+ */
+using Bank;
+
+public Account maryAccount = new Account();
+
